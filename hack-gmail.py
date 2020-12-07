a745da3e6e51137b471abe45e079acbb07e9b641
d4az <dasithvidanage000@gmail.com>
2020-12-08 00:19:39 +0530

    Update hack-gmail.py

--- hack-gmail.py
+++ hack-gmail.py
@@ -7,7 +7,7 @@ def artwork():
     print("#                     \||/                               #")
     print("#                     |  @___oo                          #")
     print("#           /\  /\   / (__,,,,|                          #")
-    print("#          ) /^\) ^\/ _)                GmailHack!       #")
+    print("#          ) /^\) ^\/ _)                Gmail-Hack!      #")
     print("#          )   /^\/   _)                CoDeD By:        #")
     print("#          )   _ /  / _)                         d4az    #")
     print("#      /\  )/\/ ||  | )_)                                #")
